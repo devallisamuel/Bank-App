921c222c98f89f035cc2ddb7e6fbf01ffd73e145
devallisamuel <49093655+devallisamuel@users.noreply.github.com>
2021-11-08 08:50:33 +0100

    updated changes

--- Bank App/Program.cs
+++ Bank App/Program.cs
@@ -6,7 +6,22 @@ namespace Bank_App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Profit(10, 5);
+            CostPrice(500, 10);
+            DepreciationValue(100, 0.55, 2);
+            Console.Read();
+        }
+        public static int Profit (int sales,int cost)
+        {
+            return sales - cost;
+        }
+        public static int CostPrice (int sellingPrice, int loss)
+        {
+            return sellingPrice + loss;
+        }
+        public static double DepreciationValue (int initVal, double interest, int years)
+        {
+            return initVal*(1+ interest)*years;
         }
     }
 }
